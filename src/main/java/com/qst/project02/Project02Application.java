140ff56273d1f202edd14fcd57c1d0ba334ef710
GP03ForProject2 <1904891443@qq.com>
2019-12-14 16:56:30 +0800

    登陆

--- src/main/java/com/qst/project02/Project02Application.java
+++ src/main/java/com/qst/project02/Project02Application.java
@@ -1,13 +1,15 @@
 package com.qst.project02;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//扫描的mapper
+@MapperScan("com.qst.project02.mapper")
 @SpringBootApplication
 public class Project02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project02Application.class, args);
 	}
-
 }
